6525bc291d3f1502e57776139cc893f989a1d48e
serrod <se.rod928@gmail.com>
2022-03-16 20:43:04 -0400

    heroku fix 2

--- server.js
+++ server.js
@@ -19,6 +19,16 @@ const MongoClient = require('mongodb').MongoClient;
 const client = new MongoClient(url);
 client.connect();
 
+if (process.env.NODE_ENV === 'production') 
+{
+  // Set static folder
+  app.use(express.static('frontend/build'));
+  app.get('*', (req, res) => 
+ {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
+
 app.get('/', function (req, res) {
   res.render('index', {});
 });
