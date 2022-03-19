19df7ce0be1748d330f0c1553614be41c5e239cf
gianfrancosg <gianfrancosantinato@hotmail.com>
2022-03-19 03:19:42 -0400

    Added Landing Page,dependencies, changed index.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { Helmet } from "react-helmet";
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
 import LoginPage from './pages/LoginPage';
 import CardPage from './pages/CardPage';
 import RegisterPage from './pages/RegisterPage';
-import LandingPage from './pages/LandingPage';
+import LandingPage from './pages/LandingPage.jsx';
 function App() {
   return (
     <Router >
+      <Helmet>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Khula:wght@400;600;800&display=swap" rel="stylesheet" />
+      </Helmet>
       <Switch>
         <Route path="/" exact>
           <LandingPage />
