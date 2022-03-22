5507149da5b52b868ea6dad7cc6b0f5a9bc780e8
gianfrancosg <gianfrancosantinato@hotmail.com>
2022-03-21 21:26:47 -0400

    Changes done on Testing.js

--- frontend/src/components/Testing.js
+++ frontend/src/components/Testing.js
@@ -6,13 +6,17 @@ function Testing()
    return(
     <div class="containerL">
         <div class="leftPanel">
-            <img className="radius8" class="pictureSizeLogin" src={LoginImage} alt="chef" style={{zIndex: 9}} />
+            <img className="radius8" class="pictureSizeLogin" src={LoginImage} alt="chef" />
         </div>
         <div class="rightPanel">
             <div class="LoginText">
-            <span id="inner-title">Please Log In</span><br />
-            <input type="text" id="loginName" placeholder="Username"/><br />
-            <input type="password" id="loginPassword" placeholder="Password"/><br />
+            <span id="inner-title" class="loginTitle">FoodGram</span><br />
+            <div class="form-group">
+                <input type="text" class="form-control" id="loginName" placeholder="Username"/>
+            </div>
+            <div class="form-group ">
+                <input type="password" class="form-control" id="loginPassword" placeholder="Password"/>
+            </div>
             <input type="submit" id="loginButton" class="buttons" value = "Do It"/>
             <input type="button" id="register" class="buttons" value = "Sign up!"
                 onClick={event =>  window.location.href='/register'} />
