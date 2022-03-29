65dd53aef310e42495a868510df2bc6ba6e34096
SaharaSheik <sahara_sheik@hotmail.com>
2022-03-29 14:31:31 -0400

    server.js updated with pass validator and mongo

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 
 const express = require('express');
+var passport=require('passport');
+
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const path = require('path');           
+
+
 const PORT = process.env.PORT || 5000;  
 
 const app = express();
@@ -14,13 +18,31 @@ app.use(cors());
 app.use(bodyParser.json());
 
 require('dotenv').config();
-const url = process.env.MONGODB_URI;
-const MongoClient = require('mongodb').MongoClient;
-const client = new MongoClient(url);
-client.connect();
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
-var api = require('./api.js');
-api.setApp( app, client );
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/foodgram');//process.env.MONGODB_URI;
+mongoose.set('debug', true);
+var User=require('./models/User');
+const { Redirect } = require('react-router-dom');
+
+passport.use(new LocalStrategy({
+  usernameField: 'login',
+  passwordField: 'password'
+}, function(login, password, done) {
+  User.findOne({Login: login}).then(function(user){
+    console.log(user);
+    if(!user || !user.validPassword(password)){
+      return done(null, false, {errors: {'email or password': 'is invalid'}});
+    }
+
+    return done(null, user);
+  }).catch(done);
+}));
+
+
 
 if (process.env.NODE_ENV === 'production') 
 {
@@ -31,6 +53,8 @@ if (process.env.NODE_ENV === 'production')
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
+
+
 
 app.use((req, res, next) => 
 {
