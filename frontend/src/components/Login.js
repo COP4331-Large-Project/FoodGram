0ef19ece2424beb4038e483244a8e19d5a22e400
gianfrancosg <gianfrancosantinato@hotmail.com>
2022-03-29 13:43:07 -0400

    repushing

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -81,7 +81,7 @@ function Login()
                 <p class="w-100 text-center">Don't have an account? {element}</p>
               </div>
           </form>
-          <span id="loginResult" class="textCenter">{message}</span>
+          <a id="loginResult" class="textCenter">{message}</a>
         </div>
       </div>
     );
