3b2737c400c8a5bf5e1e798ee6a8229771982e4a
serrod <se.rod928@gmail.com>
2022-03-29 15:05:46 -0400

    Revert "api, forget password added"
    This reverts commit 0ba83e262a2a30e6da5fed8cf5680385aa3ef7ca.

--- api.js
+++ api.js
@@ -1,85 +1,51 @@
-passport.use(new LocalStrategy({
-    usernameField: 'login',
-    passwordField: 'password'
-  }, function(login, password, done) {
-    User.findOne({Login: login}).then(function(user){
-      console.log(user);
-      if(!user || !user.validPassword(password)){
-        return done(null, false, {errors: {'email or password': 'is invalid'}});
-      }
-  
-      return done(null, user);
-    }).catch(done);
-  }));
+require('express');
+require('mongodb');
 
+exports.setApp = function (app, client) {
 
+    app.post('/api/register/', async (req, res, next) => {
+        const { FirstName, LastName, Login, Password , Email} = req.body;
+        const newUser = { FirstName: FirstName, LastName: LastName, Login: Login, Password: Password, Email: Email};
+        var error = '';
 
-app.post('/api/register/', async (req, res, next) =>
-{
-  // incoming: firstName, lastName, login, password
-  // outgoing: error
-  console.log(req.body);
-  const { FirstName, LastName , Login , Password , Email} = req.body;
-
-  const results = User.findOne({ Login: Login, Password: Password }).toArray();
-  if(results.length > 0)
-    {
+        const db = client.db('foodgram');
+        const results = await db.collection('users').find({ Login: Login, Password: Password }).toArray();
+        
+        if(results.length > 0)
+        {
             error = 'User already exists';
-    }
-  else
-    {
-      console.log(FirstName,LastName);
-      var user = new User();
-      user.FirstName=FirstName;
-      user.LastName=LastName;
-      user.Login=Login;
-      user.Email=Email;
-      console.log("kkk",user);
-    
-      user.setPassword(Password);
-      console.log("lll",user);
-      user.save().then(function(){
-        return res.json({user: user.toAuthJSON()});
-      }).catch(next);
-    }
-    var ret = { error: error };
-    res.status(200).json(ret);
-});
+        }
+        else
+        {
+            const result = db.collection('users').insertOne(newUser);
+            error = '';
+        }
 
+        var ret = { error: error };
+        res.status(200).json(ret);
+    });
 
-app.post('/api/login/', async (req, res, next) => 
-{
-  const { login, password } = req.body;
-  console.log(login,password);
+    app.post('/api/login/', async (req, res, next) => {
+        // incoming: login, password
+        // outgoing: id, firstName, lastName, error
+        var error = '';
 
-  passport.authenticate('local', {session: false}, function(err, user, info){
-    if(err){ return next(err); }
-    console.log(user);
-    if(user){
-      user.token = user.generateJWT();
-      return res.json({user: user.toAuthJSON()});
-    } else {
-     // return res.status(422).json(info);
-      res.redirect('/api/login');
-    }
-  })(req, res, next);
+        const { login, password } = req.body;
 
-});
+        const db = client.db('foodgram');
+        const results = await db.collection('users').find({ Login: login, Password: password }).toArray();
 
-app.post('/api/forgetpassword/', async (req, res, next) => 
-{
-  const { email, new_password,confirm_password } = req.body;
-  if(new_password!=confirm_password) return res.status(422).json({errors: {password: "not match"}});
+        var id = -1;
+        var fn = '';
+        var ln = '';
 
-  var user=await User.findOne({Email: email});
-  var new_user=user;
-  if (user) {
-  await user.deleteOne();
-  new_user.setPassword(new_password);
-  new_user.save();
-  res.redirect('/api/login');
-  }
-  else {
-    return res.status(422).json({errors: {password: "Email not exist"}});
-  }
-});  
+        if (results.length > 0) {
+            id = results[0].UserId;
+            fn = results[0].FirstName;
+            ln = results[0].LastName;
+        }
+
+        var ret = { id: id, firstName: fn, lastName: ln, error: '' };
+        res.status(200).json(ret);
+    });
+}
