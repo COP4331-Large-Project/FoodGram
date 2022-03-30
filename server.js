b11ce0b6f9a4bc9f763daf18918824088c4d2454
Sahar Sheikholeslami <84471457+SaharaSheik@users.noreply.github.com>
2022-03-30 01:46:10 -0400

    Update server.js
    updated server js

--- server.js
+++ server.js
@@ -18,31 +18,9 @@ app.use(cors());
 app.use(bodyParser.json());
 
 require('dotenv').config();
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
 
-var mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/foodgram');//process.env.MONGODB_URI;
-mongoose.set('debug', true);
-var User=require('./models/User');
-const { Redirect } = require('react-router-dom');
-
-passport.use(new LocalStrategy({
-  usernameField: 'login',
-  passwordField: 'password'
-}, function(login, password, done) {
-  User.findOne({Login: login}).then(function(user){
-    console.log(user);
-    if(!user || !user.validPassword(password)){
-      return done(null, false, {errors: {'email or password': 'is invalid'}});
-    }
-
-    return done(null, user);
-  }).catch(done);
-}));
-
-
+var api = require('./api.js');
+api.setApp( app);
 
 if (process.env.NODE_ENV === 'production') 
 {
