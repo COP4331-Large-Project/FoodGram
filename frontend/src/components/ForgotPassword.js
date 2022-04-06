66504c566d1f5daae7a399628832c65b72a0fe9d
gianfrancosg <gianfrancosantinato@hotmail.com>
2022-04-06 17:44:00 -0400

    Added Forgot Password frontend page design

--- frontend/src/components/ForgotPassword.js
+++ frontend/src/components/ForgotPassword.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import LoginImage from "../assets/img/chef_forgotpassword.png";
 
 function ForgotPassword(){
         var _ud = localStorage.getItem('user_data');
@@ -13,11 +14,21 @@ function ForgotPassword(){
             window.location.href = '/';
         };    
       return(
-       <div id="loggedInDiv">
-       <span id="userName">Logged In As {firstName} {lastName}</span><br />
-       <button type="button" id="logoutButton" class="buttons" 
-         onClick={doLogout}> Log Out </button>
-       </div>
+        <div id="forgotPasswordDiv">
+        <div class="leftPanel">
+            <img className="radius8" class="pictureSizeForgotPassword" src={LoginImage} alt="forgot-password"/>
+        </div>
+        <div class="LoginText">
+            {/* <form onSubmit={doRegister}> */}
+                <div class="form-group">
+                  <h2 id="text" style={{textAlign: "center"}}>Please input the email address associated with your account.</h2><br/>
+                  <input type="text" class="form-control col-md-10" placeholder="Email" />
+                </div>
+                <input type="submit" class="form-controlL btn-danger submit col-md-10" value = "Submit"/>
+            {/* </form> */}
+            <span id="registerResult"></span>
+        </div>
+     </div>
       );
 };
 export default ForgotPassword;
