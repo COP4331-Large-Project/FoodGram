cc38f9dc46e1bedbd47af0b057ffad70ba793c99
serrod <se.rod928@gmail.com>
2022-04-05 20:44:15 -0400

    Revert "Merge branch 'test'"
    This reverts commit cae70d995e5dcb42ef386c23ac505f628c92dada, reversing
    changes made to 6f3d34af368277e7fc78f928267ba9a12afd3e9c.

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 
 const express = require('express');
-var passport=require('passport');
-
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const multer = require('multer');
 
 const path = require('path');           
-
-
 const PORT = process.env.PORT || 5000;  
 
 const app = express();
@@ -19,9 +15,13 @@ app.use(cors());
 app.use(bodyParser.json());
 
 require('dotenv').config();
+const url = process.env.MONGODB_URI;
+const MongoClient = require('mongodb').MongoClient;
+const client = new MongoClient(url);
+client.connect();
 
 var api = require('./api.js');
-api.setApp( app);
+api.setApp( app, client );
 
 if (process.env.NODE_ENV === 'production') 
 {
@@ -32,8 +32,6 @@ if (process.env.NODE_ENV === 'production')
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
-
-
 
 app.use((req, res, next) => 
 {
