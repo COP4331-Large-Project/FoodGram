4787380d7d32a6e608b191e3d65aa0fb345cc83c
gianfrancosg <gianfrancosantinato@hotmail.com>
2022-04-15 12:09:28 -0400

    Fixing padding issue with the message on ForgotPassword

--- frontend/src/components/ForgotPassword.js
+++ frontend/src/components/ForgotPassword.js
@@ -34,21 +34,23 @@ function ForgotPassword(){
   
 
       return(
-        <div id="forgotPasswordDiv">
+      <div id="forgotPasswordDiv">
         <div class="leftPanel">
             <img className="radius8" class="pictureSizeForgotPassword" src={ForgotPasswordImage} alt="forgot-password"/>
         </div>
-        <div class="LoginText">
+        <div class="ForgotPasswordText">
             <form onSubmit={doForgotPassword}>
                 <div class="form-group">
-                  <h2 id="text" style={{textAlign: "center"}}>Please input the email address associated with your account.</h2><br/>
+                  <h2 id="text" style={{textAlign: "center", marginLeft:"-60px"}}>Please input the email address associated with your account.</h2><br/>
                   <input id="forgotPasswordEmail" type="text" class="form-control col-md-10" placeholder="Email" ref={(c) => forgotPasswordEmail = c}/>
                 </div>
                 <input type="submit" class="form-controlL btn-danger submit col-md-10" value = "Submit" onClick={doForgotPassword}/>
             </form>
             <br/>
-            <span id="forgotPasswordResult" class="w-100 text-center">{message}</span>
+            <span class="w-100 text-center" style={{marginLeft:"-60px"}}>Hello!</span>
+            <span id="forgotPasswordResult" class="w-100 text-center" style={{marginLeft:"-60px"}}>{message}</span>
         </div>
+
      </div>
       );
 };
