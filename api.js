5d94253399d8f4e2420c22b51cc280eabc1e8d5e
chanderson7 <charlieanderson2001@gmail.com>
2022-04-19 12:14:01 -0400

    Duplicate search api fix, got it during a pull

--- api.js
+++ api.js
@@ -351,36 +351,5 @@ app.post('/api/search/', async function(req, res, next) {
 
 });
 
-app.post('/api/search/', async function(req, res, next) {
-
-  const { postID, userID } = req.body;
-
-  imgModel.find({
-    $or: [
-      {
-        "name": {'$regex': search}
-      },
-      {
-        "recipe": {'$regex': search}
-      }, 
-      {
-        "category": {'$regex': search}
-      }, 
-    ]
-  }
-  
-  , function(err, result) {
-
-    if (err) {
-      console.log(err);
-    } else {
-      res.json(result);
-    }
-  });
-
-  console.log(search);
-
-});
-
 
 }
