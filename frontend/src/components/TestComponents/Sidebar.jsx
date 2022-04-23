b0042caa1e90d57a58f751576a9cbafbabed5bdb
Edelis <edelitamr@gmail.com>
2022-04-23 16:25:29 -0400

    Added sidebar placeholders

--- frontend/src/components/TestComponents/Sidebar.jsx
+++ frontend/src/components/TestComponents/Sidebar.jsx
@@ -1,12 +1,74 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import {
+  AccountBox,
+  Article,
+  Group,
+  Home,
+  ModeNight,
+  Person,
+  Settings,
+  Storefront,
+  Info,
+} from "@mui/icons-material";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Switch,
+} from "@mui/material";
+import React from "react";
 
-const Sidebar = () => {
+const Sidebar = ({ mode, setMode }) => {
   return (
-    <Box bgcolor="skyblue" flex={1} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
-      Sidebar      
+    <Box flex={1} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
+      <Box position="fixed">
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#home">
+              <ListItemIcon>
+                <Home />
+              </ListItemIcon>
+              <ListItemText primary="My Recipes" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemIcon>
+                <Article />
+              </ListItemIcon>
+              <ListItemText primary="Explore" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemIcon>
+                <AccountBox />
+              </ListItemIcon>
+              <ListItemText primary="Profile" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemIcon>
+                <Info />
+              </ListItemIcon>
+              <ListItemText primary="About Us" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemIcon>
+                <ModeNight />
+              </ListItemIcon>
+              <Switch onChange={(e) => setMode(mode === "light" ? "dark" : "light")} />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
