c982346db7b93d5e10081657e498dfc67cd2b860
Edelis <edelitamr@gmail.com>
2022-04-24 19:24:08 -0400

    Footer component

--- frontend/src/components/HomePageComponents/Footer.jsx
+++ frontend/src/components/HomePageComponents/Footer.jsx
@@ -1,7 +1,28 @@
-import { Container, Grid, Box, } from "@mui/material";
+import { Container, Grid, Box } from "@mui/material";
+import { Typography } from "@mui/material";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {"Copyright © "}
+      Group 22 Large Project Spring {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 
 const Footer = () => {
-    return <div>Footer</div>
+  return (
+    <Box sx={{ bgcolor: "skyblue"}} component="footer">
+      <Typography variant="h6" align="center">
+        FoodGram
+      </Typography>
+      <Typography variant="subtitle1" align="center" color="text.secondary" component="p">
+        Let's grab a coffee. Viva las Arepas!
+      </Typography>
+      <Copyright />
+    </Box>
+  );
 };
 
 export default Footer;
