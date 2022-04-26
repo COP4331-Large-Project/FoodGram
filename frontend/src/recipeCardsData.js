f945d3196d698bc42d43ce06c601c38efa7c0911
serrod <se.rod928@gmail.com>
2022-04-25 20:49:06 -0400

    Revert "Revert "Footer component""
    This reverts commit 748873b0a86a71515295aa3148c30b74ef49017d.

--- frontend/src/recipeCardsData.js
+++ frontend/src/recipeCardsData.js
@@ -51,4 +51,56 @@ export const recipeCards = [
     description:
       "This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.",
   },
+  {
+    image: "/Assets/paella.jpg",
+    title: "Shrimp Paella",
+    description:
+      "This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.",
+  },
+  {
+    image: "/Assets/sangria.jpg",
+    title: "Sangria",
+    description:
+      "When you need a crowd-pleasing cocktail, sangria is the obvious choice. A pitcher of sangria is the perfect summer drink to sip on at your backyard barbecue or party. So, let us introduce you to the best sangria recipe on the internet. It's easy to make and, even better, it's irresistibly delicious.",
+  },
+  {
+    image: "/Assets/salsa.jpg",
+    title: "Secret Salsa",
+    description: "Sweet and spicy pico de gallo-style salsa with my secret ingredient! ",
+  },
+  {
+    image: "/Assets/steak.jpg",
+    title: "Rosemary Steak",
+    description:
+      "This is a wonderful steak dish is quite popular in various restaurants around Buenos Aires. ",
+  },
+  {
+    image: "/Assets/paella.jpg",
+    title: "Shrimp Paella",
+    description:
+      "This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.",
+  },
+  {
+    image: "/Assets/sangria.jpg",
+    title: "Sangria",
+    description:
+      "When you need a crowd-pleasing cocktail, sangria is the obvious choice. A pitcher of sangria is the perfect summer drink to sip on at your backyard barbecue or party. So, let us introduce you to the best sangria recipe on the internet. It's easy to make and, even better, it's irresistibly delicious.",
+  },
+  {
+    image: "/Assets/salsa.jpg",
+    title: "Secret Salsa",
+    description: "Sweet and spicy pico de gallo-style salsa with my secret ingredient! ",
+  },
+  {
+    image: "/Assets/steak.jpg",
+    title: "Rosemary Steak",
+    description:
+      "This is a wonderful steak dish is quite popular in various restaurants around Buenos Aires. ",
+  },
+  {
+    image: "/Assets/paella.jpg",
+    title: "Shrimp Paella",
+    description:
+      "This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.",
+  },
 ];
