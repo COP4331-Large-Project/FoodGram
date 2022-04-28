caeb056ddc618f644d77475769b7b0010d93e350
serrod <se.rod928@gmail.com>
2022-04-28 19:16:52 -0400

    added wip unit testing changes

--- api.test.js
+++ api.test.js
@@ -1,5 +1,10 @@
-const  api = require('./api');
-const request = require('supertest')
+const  app = require('./server');
+const request = require('supertest');
+const supertest = require('supertest');
+const http = require('http');
+
+
+//const apptest = supertest(http.createServer(app));
 
 test('adds 1 + 2 to equal 3', () => {
     expect(1+2).toBe(3);
@@ -7,14 +12,40 @@ test('adds 1 + 2 to equal 3', () => {
 
 describe("login tests", () => {
   test("invalid username",async() =>{
-    await request(api).post("/api/login")
+    const response = await request(app).post("localhost:5000/api/login")
     .send({
       login: "does not exist",
       password: "imnotreal"
-      })
-      .expect((res) => {
-        res.body.status = 500;
-        res.body.error = "Login/Password Invalid"
-      })
+    })
+    expect(response.statusCode).toBe(500);  
   })
 })
+/*
+describe("register tests", () => {
+  test("passwords dont match",async() =>{
+    await request(app).post("/api/register")
+    .send({
+      login: "does not exist",
+      password: "imnotreal"
+    })
+    expect((res) => {
+      res.body.status = 500;
+      res.body.error = "Login/Password Invalid"
+    })
+  })
+})
+
+describe("reset-password tests", () => {
+  test("passwords dont match",async() =>{
+    await request(api).post("/api/forgot-password")
+    .send({
+      login: "does not exist",
+      password: "imnotreal"
+    })
+    .expect((res) => {
+      res.body.status = 500;
+      res.body.error = "Login/Password Invalid"
+    })
+  })
+})
+*/
