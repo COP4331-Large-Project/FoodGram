a0ba9624a64b03fe10ae4493e097d0e32e0d1803
cris-me <soda369xd@gmail.com>
2022-04-27 20:02:49 -0400

    Image Path
    Added actualImagePath as alternative pathway

--- models/image.js
+++ models/image.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var imageSchema = new mongoose.Schema({
     name: {type:String, required:true},
     userId: {type:String, required:true},
+    actualImagePath: {type:String, required:true},
     imagePath: {type:String, required:true},
     ingredients: {type:String, required:true},
     instructions: {type:String, required:true},
