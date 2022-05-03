daeb685dcbbadeb95b580d19a5b13320ba577b32
Edelis <edelitamr@gmail.com>
2022-05-03 11:07:17 -0400

    more text to about us

--- frontend/src/components/AboutUs.jsx
+++ frontend/src/components/AboutUs.jsx
@@ -10,21 +10,25 @@ function AboutUs() {
           </div>
           <div class="content">
             <h3>We are Group 22 for COP4331 </h3>
-            <p>
+            <p className="extraBold font60">
               Group 22 was founded in March 2022, when eight students of the COP4331-Spring22 class
               teamed up and opened a channel on Discord to create a MERN stack application focused
               on Food as a central theme. As avid eaters and late-nighters, Group 22 took
               inspiration from the COT4331 class to create their first MERN stack web and mobile
               application. The idea behind this app sprouted from our love of food and community, we
               wanted to create a space that encourages connection, and what better way than to do it
-              through food. Here you can explore, engage, and create!
+              through food.
+            </p>
+            <p className="font5">
+              Here you can explore, engage, and create. The next time you visit our website, we hope
+              you find something a little extra to spice up your day.... also sponsored by Elon Musk
             </p>
             {/* <div class="button">
                <a href="">Read More</a>
              </div> */}
           </div>
           <div class="social">
-            <a href="">
+            {/* <a href="">
               <i class="fab fa-facebook-f"></i>
             </a>
             <a href="">
@@ -32,7 +36,7 @@ function AboutUs() {
             </a>
             <a href="">
               <i class="fab fa-instagram"></i>
-            </a>
+            </a> */}
           </div>
         </div>
         <div class="image-section">
