cc241de5c92c6a98e96c5b928578428e7311dca7
Edelis <edelitamr@gmail.com>
2022-05-03 13:32:16 -0400

    quick fix to about us

--- frontend/src/components/AboutUs.jsx
+++ frontend/src/components/AboutUs.jsx
@@ -6,11 +6,13 @@ function AboutUs() {
       <div class="container">
         <div class="content-section">
           <div class="title">
-            <h1>About Us</h1>
+            <h1 className="extraBold font60">About Us</h1>
           </div>
           <div class="content">
-            <h3 style={{textAlign:"center"}}>We are Group 22 for COP4331 </h3>
-            <p className="extraBold font60">
+            <h3 className="Bold font40" style={{ textAlign: "center" }}>
+              We are Group 22 for COP4331{" "}
+            </h3>
+            <p className="Bold font20">
               Group 22 was founded in March 2022, when eight students of the COP4331-Spring22 class
               teamed up and opened a channel on Discord to create a MERN stack application focused
               on Food as a central theme. As avid eaters and late-nighters, Group 22 took
@@ -23,19 +25,8 @@ function AboutUs() {
                <a href="">Read More</a>
              </div> */}
           </div>
-          <div class="social">
-            {/* <a href="">
-              <i class="fab fa-facebook-f"></i>
-            </a>
-            <a href="">
-              <i class="fab fa-twitter"></i>
-            </a>
-            <a href="">
-              <i class="fab fa-instagram"></i>
-            </a> */}
-          </div>
         </div>
-        <div class="image-section">
+        <div class="image-section" style={{ textAlign: "center" }}>
           <img className="radius8" class="pictureSizeLogin" src={LoginImage} alt="Team" />
           <br></br>
           <br></br>
